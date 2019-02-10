c1af03a7cd9c3a460146eda0cd8d8b0e6fc16bea
David Mohammed <fossfreedom@ubuntu.com>
2019-02-10 12:27:34 +0000

    Refresh from package source

--- setup.py
+++ setup.py
@@ -1,31 +1,24 @@
 from distutils.core import setup
+from DistUtilsExtra.command import *
 import glob
-import os.path
 import re
+import os
 
-from DistUtilsExtra.command import build_extra, build_i18n
-
-# Try to get a current version from changelog.
-version = None
-changelog_file_path = 'debian/changelog'
-if os.path.exists(changelog_file_path):
-    with open(changelog_file_path) as changelog_file:
-        head = changelog_file.readline()
-    match = re.match(r'.*\((.*)\).*', head)
+# look/set what version we have
+changelog = "debian/changelog"
+if os.path.exists(changelog):
+    head=open(changelog).readline()
+    match = re.compile(".*\((.*)\).*").match(head)
     if match:
         version = match.group(1)
 
 setup(
-    name='budgie-wallpapers',
-    description='Wallpapers for Ubuntu Budgie',
-    version=version,
-    url='https://github.com/UbuntuBudgie/budgie-wallpapers',
-    data_files=[
-        ('share/backgrounds/budgie', glob.glob('*.png') + glob.glob('*.jpg')),
-        ('share/backgrounds/budgie', glob.glob('contest/*.xml')),
-    ],
-    cmdclass={
-        'build': build_extra.build_extra,
-        'build_i18n': build_i18n.build_i18n,
-    },
+    name = 'budgie-wallpapers',
+    version = version,
+    data_files=[('share/backgrounds/budgie', glob.glob('*.png')+glob.glob('*.jpg')),
+		('share/backgrounds/budgie', glob.glob('contest/*.xml')),
+               ],
+    cmdclass = { "build" : build_extra.build_extra,
+                 "build_i18n" :  build_i18n.build_i18n }
 )
+
